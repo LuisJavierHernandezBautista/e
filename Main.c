924b98b07a8dfa56b550dcab70281bfa9b79f7f1
LuisJavierHernandezBautista <luisjavierhdezbta@gmail.com>
2023-07-13 09:54:19 -0600

    lectura de numeros

--- Main.c
+++ Main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-main(){
 
-
+main()
+{
+	printf("Hola mundo desde git");
+	int n1, n2;
+	printf("ingresa n1: ");
+	scanf("%d",&n1);
+	printf("ingresa n2: ");
+	scanf("%d",&n2);
+	printf("n1 = %d n2 = %d",n1,n2);
 }
